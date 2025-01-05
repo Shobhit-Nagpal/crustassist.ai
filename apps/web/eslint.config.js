af720ada7a2f0659f2c7c36b7998f5dda1402cbb
Shobhit-Nagpal <shobhitsnagpal@gmail.com>
2025-01-05 17:31:51 +0530

    init

--- apps/web/eslint.config.js
+++ apps/web/eslint.config.js
@@ -1,4 +1,4 @@
-import { nextJsConfig } from "@repo/eslint-config/next-js";
+import { nextJsConfig } from "@crustassistant/eslint-config/next-js";
 
 /** @type {import("eslint").Linter.Config} */
 export default nextJsConfig;
